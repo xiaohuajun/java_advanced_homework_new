ae1216530f32e8603de1a9c67fc4d9b68bc7b380
xiaohuajun <18061741650@163.com>
2021-10-03 18:07:05 +0800

    作业-09

---
+++ week09/rpcfx-demo/src/main/java/com/xiaohuajun/rpcfxdemo/RpcfxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohuajun.rpcfxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcfxDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RpcfxDemoApplication.class, args);
+  }
+
+}
---
+++ week09/rpcfx-demo/src/test/java/com/xiaohuajun/rpcfxdemo/RpcfxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohuajun.rpcfxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcfxDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
