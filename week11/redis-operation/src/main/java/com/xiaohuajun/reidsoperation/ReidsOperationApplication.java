160bf31df21865b13179c00f4a380b0e20f1e8b1
xiaohuajun <18061741650@163.com>
2021-10-24 17:11:22 +0800

    11期作业

---
+++ week11/redis-operation/src/main/java/com/xiaohuajun/reidsoperation/ReidsOperationApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohuajun.reidsoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidsOperationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReidsOperationApplication.class, args);
+  }
+
+}
---
+++ week11/redis-operation/src/test/java/com/xiaohuajun/reidsoperation/ReidsOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohuajun.reidsoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReidsOperationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
