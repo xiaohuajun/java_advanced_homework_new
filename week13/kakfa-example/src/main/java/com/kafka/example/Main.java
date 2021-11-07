9e919e3df721b242c0f7062a090ece1347766f9a
xiaohuajun <18061741650@163.com>
2021-11-07 21:19:23 +0800

    第13周作业

---
+++ week13/kakfa-example/src/main/java/com/kafka/example/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kafka.example;
+
+import com.kafka.example.kafka.KafkaUtil;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+/**
+ * @author lw1243925457
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String servers = "localhost:9092,localhost:9093,localhost:9094";
+        String topic = "TestTopic";
+        String message = "test";
+
+        KafkaProducer<String, String> producer = KafkaUtil.createProducer(servers);
+        KafkaUtil.send(producer, topic, message);
+
+        KafkaConsumer<String, String> consumer = KafkaUtil.createConsumer(servers, topic);
+        KafkaUtil.readMessage(consumer, 100);
+    }
+}
